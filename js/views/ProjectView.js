9607848091db09c5f8f7649a9568212335f12c8b
Vishaal Kalwani <vishaalk@umich.edu>
2014-06-03 22:05:01 -0700

    Changed the individual project view a bit

--- js/views/ProjectView.js
+++ js/views/ProjectView.js
@@ -3,7 +3,29 @@ var ProjectView = Backbone.View.extend({
 	//template: _.template('<b>  <%= title %> </b>: <p> <%= description %> </p>  Is drop in?: <%= (isDropIn) ? "Yes" : "No" %>'),
 	
 	className: 'row info',
-	template: _.template('<div class="col-md-2">  Is Drop In?: <%= (isDropIn) ? "Yes" : "No" %> <p> maxSignups: <%= maxSignups %> </p></div><div class="col-md-10"> <%= title %> </b>: <p> <%= description %> </p> </div>'),
+	template: _.template('<div class="media" style="padding: 4px 4px;"> <a class="pull-left" href="#"> <img class="media-object" src="http://placehold.it/64x64"> </a> \
+							<div class="media-body"> \
+								<h6 class="media-heading"> <%= title %> </h6> <div class="pull-right"> hello </div>\
+								<%= description %> \
+								<div class="pull-right"> \
+								Is Drop In? <%= (isDropIn) ? "Yes" : "No" %> <br> \
+								Max Signups: <%= maxSignups %> \
+								</div> \
+							</div> \
+							</div>'),
+	template: _.template('<div class="media" style="padding: 4px 4px;"> <a class="pull-left" href="#"> <img class="media-object" src="http://placehold.it/64x64"> </a> \
+							<div class="media-body"> \
+								<div class="col-md-8"> \
+									<b><%= title %></b><br> \
+									<p><%= description %></p> \
+								</div> \
+								<div class="col-md-2"> \
+									<%= maxSignups %> \
+								</div> \
+							</div> \
+						</div>'),
+
+	//template: _.template('<div class="col-md-2">  Is Drop In?: <%= (isDropIn) ? "Yes" : "No" %> <p> maxSignups: <%= maxSignups %> </p></div><div class="col-md-10"> <%= title %> </b>: <p> <%= description %> </p> </div>'),
 
 	events: {
 		'dblclick' : 'edit' 
