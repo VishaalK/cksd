923d32c3ab6a1461b75f340832d1a561def43561
Vishaal Kalwani <vishaalk@umich.edu>
2014-06-16 22:45:12 -0700

    Added a basic Alumni page, populate with a View later

---
+++ alumni.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title> Service Day | Projects </title>
+        <meta charset="utf-8"></head>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/sticky-footer.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css" />
+        <style>
+	        body {
+	            margin-top: 20px;
+	            background-color: #CAEFFA;
+	        }
+	        navbar {
+	            margin-bottom: 20px;
+	        }
+	        #Banner {
+	        	margin-bottom: 20px;
+	        }
+	        #addProject {
+	        	margin-bottom: 5px;
+	        }
+	        #ProjectsContainer {
+	        }
+        </style>
+	</head>	
+	<body>
+	<script type="text/template" id="NavbarTemplate">
+	    <nav class="navbar navbar-default" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Circle K</a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="#">Home</a></li>
+		        <li><a href="#">About</a></li>
+		        <li><a href="#">Calendar</a></li>
+		        <li><a href="#">Leadership</a></li>
+		        <li><a href="#">Join Now</a></li>
+		        <li><a href="#">House Cup</a></li>
+		        <li><a href="#">Long Term Volunteer Positions</a></li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Vishaal Kalwani<span style="margin-left: 5px" class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">Separated link</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</script>	
+	<header class="container" id="NavbarContainer">
+	</header>
+    		
+    <div class="container" id="AlumniContainer">
+
+    </div>  
+
+    <script type="text/template" id="AlumniTemplate">
+
+    </script>
+
+
+    <footer>
+        <div class="container">
+        <div class="row">
+        <div class="col-md-4 column">
+        	<p class="center-block footer-information">
+        		Sint bitters beard skateboard fugiat ad VHS. Quis cupidatat keffiyeh umami food
+        truck, art party 
+        	</p>
+        </div>
+        <div class="col-md-4 column"><p class="center-block">
+        	Sint bitters beard skateboard fugiat ad VHS. Quis cupidatat keffiyeh umami food
+        truck, art party
+        </p></div>
+        <div class="col-md-4 column"><p class="center-block">
+        	Sint bitters beard skateboard fugiat ad VHS. Quis cupidatat keffiyeh umami food
+        truck, art party
+        </p></div>
+        </div>
+    </footer>
+
+	<!--<iframe
+	width="600"
+	height="450"
+	frameborder="0" style="border:0"
+	src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBE-XXBUF3En8bk2p-PTvZYQnzXOvKT1-8
+	&q=Space+Needle,Seattle+WA">
+	</iframe> -->
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> <!-- jQuery -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"> </script> <!-- underscore.js -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js"> </script> <!-- backbone.js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/utils/backbone.localStorage.js"></script>
+    <script type="text/javascript" src="js/utils/moment.min.js"></script>
+	<script type="text/javascript" src="js/utils/bootstrap-datetimepicker.min.js"></script>
+    <script src="js/views/Navbar.js"></script>
+	</body>
+
+</html>	
