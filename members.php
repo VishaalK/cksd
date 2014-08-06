d4f545da7af5c06f084b0b35d1040df912c6ad61
Vishaal Kalwani <vishaalk@umich.edu>
2014-08-06 19:14:36 -0400

    Added preliminary members page, gotta download the data from the prod db

---
+++ members.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html5>
+<?php
+require_once('lib/db_connect.php');
+?>
+<html lang="en">
+    <head>
+        <title> All Members </title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <style>
+        body {
+            padding-top: 50px;
+        }
+        </style>
+    </head>
+    <body>
+        <?php require_once('lib/header.php'); ?>
+        <div class="page-header">
+            <h2> All Members <small> All Years </small> </h2>
+        </div> 
+    </body>
+</html>
